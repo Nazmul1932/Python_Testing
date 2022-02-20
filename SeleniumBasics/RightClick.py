0f0d84bc3ab522cdec7f914595021fe13d3f923d
nazmul <bsse1032@iit.du.ac.bd>
2022-02-20 22:31:03 +0600

    kisu na

---
+++ SeleniumBasics/RightClick.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import Select
+import time
+
+driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+driver.implicitly_wait(10)
+driver.get("http://swisnl.github.io/jQuery-contextMenu/demo.html")
+
+right_click_element = driver.find_element(By.XPATH, "//span[text()='right click me']")
+actionChains = ActionChains(driver)
+actionChains.context_click(right_click_element).perform()
+right_click_options_list = driver.find_elements(By.CSS_SELECTOR, "li.context-menu-icon span")
+
+for ele in right_click_options_list:
+    print(ele.text)
+    if ele.text == 'Copy':
+        ele.click()
+        break
+
+time.sleep(3)
+driver.quit()
